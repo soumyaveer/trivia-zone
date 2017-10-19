d0d64a4dd99a1b946a9b749333be199e4d8c96ba
Soumya Veer <veer.soumya@gmail.com>
2017-10-18 19:01:56 -0700

    add relationship tests for User#model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,4 +26,32 @@ describe User do
       expect(user.valid?).to eql(true)
     end
   end
+
+  describe 'relationship' do
+    let(:user) { User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'testpass123')}
+
+    let (:topic) do
+      Topic.create(name: Faker::Lorem.word)
+    end
+
+    before do
+      @trivia1 = Trivia.create(title: Faker::Lorem.sentence,
+                    description: Faker::Lorem.paragraph,
+                    topic: topic,
+                    user: user)
+
+      @trivia2 = Trivia.create(title: Faker::Lorem.sentence,
+                               description: Faker::Lorem.paragraph,
+                               topic: topic,
+                               user: user)
+
+      @trivia_session1 = TriviaSession.create(trivia: @trivia1, user: user, score: 30)
+      @trivia_session2 = TriviaSession.create(trivia: @trivia2, user: user, score: 70)
+
+    end
+
+    it 'has many trivia' do
+      expect(user.trivias).to match_array([@trivia1, @trivia2])
+    end
+  end
 end
