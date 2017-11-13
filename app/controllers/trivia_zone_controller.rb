d534da482fb9f433d94c176856173a1de989311d
Soumya Veer <veer.soumya@gmail.com>
2017-11-13 15:04:45 -0800

    add sign_in_or_sign_up and homepage partials to TriviaZone

--- app/controllers/trivia_zone_controller.rb
+++ app/controllers/trivia_zone_controller.rb
@@ -1,4 +1,6 @@
 class TriviaZoneController < ApplicationController
   def index
+    @topics = Topics.all
+    @trivias = current_user.trivia_sessions
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'trivia_zone#index'
-  resources :trivia_zone, only: [:index]
+  resources :trivia_zone, only: [:index, :show]
+
   resources :topics do
     resources :trivias do
       member do
