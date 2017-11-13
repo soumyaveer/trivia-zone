da4c91311a5ce2192fd6b08b4903d6c55f710aac
Soumya Veer <veer.soumya@gmail.com>
2017-11-13 14:29:14 -0800

    add load_topic and load_trivia methods to TriviasController

--- app/controllers/trivias_controller.rb
+++ app/controllers/trivias_controller.rb
@@ -1,16 +1,27 @@
 class TriviasController < ApplicationController
+  before_action :load_trivia, only: [:show, :result]
+
   def index
     @topic = Topic.find_by(id: params[:topic_id])
     @trivias = @topic.trivias
   end
 
   def show
-    @topic = Topic.find_by(id: params[:topic_id])
-    @trivia = Trivia.find(params[:id])
     @questions = @trivia.questions
   end
 
   def result
-    
+    # calculate and add results here
+  end
+
+  private
+
+  def load_topic
+    @topic = Topic.find_by(id: params[:topic_id])
+  end
+
+  def load_trivia
+    load_topic
+    @trivia = Trivia.find(params[:id])
   end
 end
