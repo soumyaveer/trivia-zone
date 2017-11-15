e79e1c92aa2d559a0bc49f6344da2102d5901b08
Soumya Veer <veer.soumya@gmail.com>
2017-11-14 16:03:11 -0800

    fix logout link

--- app/controllers/trivia_zone_controller.rb
+++ app/controllers/trivia_zone_controller.rb
@@ -1,6 +1,6 @@
 class TriviaZoneController < ApplicationController
   def index
     @topics = Topic.all
-    @trivia_sessions = current_user.trivia_sessions
+    @trivia_sessions = current_user.trivia_sessions if current_user
   end
 end
