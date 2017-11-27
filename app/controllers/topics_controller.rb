4007536edf20f1465963c2dce1c703609064c3da
Soumya Veer <veer.soumya@gmail.com>
2017-11-27 13:05:03 -0800

    add create topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,6 +3,20 @@ class TopicsController < ApplicationController
     @topics = Topic.all
   end
 
+  def new
+    @topics = Topic.all
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topics_path
+    else
+      render :index
+    end
+  end
+
   private
   def topic_params
     params.require(:topic).permit(:name)
