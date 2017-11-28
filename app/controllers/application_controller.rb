05dc46fe46833779f9edf51eabdde7a94163af83
Soumya Veer <veer.soumya@gmail.com>
2017-11-28 12:10:27 -0800

    fix redirect in case user in not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_sanitized_params, if: :devise_controller?
+  before_action :authenticate_user!
 
   def update_sanitized_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
