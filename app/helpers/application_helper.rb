269269b3d580489423fffbd526f0021200e296bf
Soumya Veer <veer.soumya@gmail.com>
2017-11-28 12:52:07 -0800

    add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def nav_status(nav_item)
+    nav_item == @current_nav_item ? 'active' : 'inactive'
+  end
+
+  def nav_item_url(nav_item)
+    nav_item == @current_nav_item ? '#' : self.nav_item_urls[nav_item]
+  end
+
+  def nav_item_urls
+    {
+      home: root_url,
+      play_trivia: topics_url,
+      leaderboard: leaderboards_path
+    }
+  end
 end
