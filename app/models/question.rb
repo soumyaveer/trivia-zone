7a0ac5f09d3c0ec4904936daef8a58dd8f42b8e4
Soumya Veer <veer.soumya@gmail.com>
2017-11-30 13:19:15 -0800

    add validation to check that there is atleast one correct answer in Question#model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,11 @@ class Question < ActiveRecord::Base
   accepts_nested_attributes_for :answers
 
   validates :description, presence: true
+  validate :there_is_atleast_one_correct_answer
   # TODO: validate that there is atleast one correct answer specified
+
+  def there_is_atleast_one_correct_answer
+    correct_answers = self.answers.where(correct: true)
+    correct_answers.count < 1 ? true : false
+  end
 end
