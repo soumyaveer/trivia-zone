cd408062846e8f98e2f2ee6be9748cd5a49a8b75
Soumya Veer <veer.soumya@gmail.com>
2017-12-01 12:05:56 -0800

    add tests for index

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -7,5 +7,30 @@ describe TopicsController do
         expect(response).to redirect_to(new_user_session_url)
       end
     end
+
+    context 'when user is logged in' do
+      before do
+        @current_user = FactoryGirl.create(:user)
+        @another_user = FactoryGirl.create(:user)
+        sign_in(:user, @current_user)
+
+      end
+
+      it 'should render template index' do
+        get :index
+
+        expect(response).to render_template('index')
+      end
+
+      it 'returns all the topics' do
+        topic1 = FactoryGirl.create(:topic)
+        topic2 = FactoryGirl.create(:topic)
+        topic3 = FactoryGirl.create(:topic)
+
+        get :index
+
+        expect(assigns("topics")).to match_array([topic1, topic2, topic3])
+      end
+    end
   end
 end
