2ded1497fefd30b975ebb9bd7564318a955d19b5
Soumya Veer <veer.soumya@gmail.com>
2017-12-01 13:02:52 -0800

    add test for destory action for TriviaSessions#Controller spec

--- spec/controllers/trivia_sessions_controller_spec.rb
+++ spec/controllers/trivia_sessions_controller_spec.rb
@@ -61,4 +61,24 @@ describe TriviaSessionsController do
       expect(response).to render_template('show')
     end
   end
+
+  describe 'DELETE destroy' do
+    let(:current_user) { FactoryGirl.create(:user) }
+
+    before do
+      sign_in(:user, current_user)
+    end
+
+    it 'should delete the trivia session' do
+      topic = FactoryGirl.create(:topic)
+      trivia = create_trivia(10, topic)
+      trivia_session_1 = create_trivia_session_with(4, current_user, trivia) # score 66
+      trivia_session_2 = create_trivia_session_with(10, current_user, trivia) # score 100
+      expect do
+        delete :destroy, params: {trivia_id: trivia.id, id: trivia_session_1.id}
+        end.to change(TriviaSession, :count).by(-1)
+
+      expect(assigns(:trivia_sessions)).not_to match_array([trivia_session_1, trivia_session_2])
+    end
+  end
 end
