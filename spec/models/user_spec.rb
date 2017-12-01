93c32035782e0babb8576eeb6304d06849e76408
Soumya Veer <veer.soumya@gmail.com>
2017-12-01 13:42:38 -0800

    fix User#Model test for user-trivia relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,30 +28,22 @@ describe User do
   end
 
   describe 'relationship' do
-    let(:user) { User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'testpass123')}
+    let(:author) { User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'testpass123')}
 
     let (:topic) do
       Topic.create(name: Faker::Lorem.word)
     end
 
     before do
-      @trivia1 = Trivia.create(title: Faker::Lorem.sentence,
-                    description: Faker::Lorem.paragraph,
-                    topic: topic,
-                    user: user)
+      @trivia1 = create_trivia(6, topic)
+      @trivia2 = create_trivia(6, topic)
 
-      @trivia2 = Trivia.create(title: Faker::Lorem.sentence,
-                               description: Faker::Lorem.paragraph,
-                               topic: topic,
-                               user: user)
-
-      @trivia_session1 = TriviaSession.create(trivia: @trivia1, user: user, score: 30)
-      @trivia_session2 = TriviaSession.create(trivia: @trivia2, user: user, score: 70)
-
+      @trivia_session1 = create_trivia_session_with(3, author, @trivia1)
+      @trivia_session2 = create_trivia_session_with(8, author, @trivia2)
     end
 
     it 'has many trivia' do
-      expect(user.trivias).to match_array([@trivia1, @trivia2])
+      expect(author.trivias).to match_array([@trivia1, @trivia2])
     end
   end
 
