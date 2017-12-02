d80cd74fb8ef414741acd30631d0e35095a107e2
Soumya Veer <veer.soumya@gmail.com>
2017-12-01 22:49:17 -0800

    Minor code cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable ,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   validates :name, :password, presence: true
 
