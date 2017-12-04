ecbb642f8bd41d77287699bdd69f853112912c11
Soumya Veer <veer.soumya@gmail.com>
2017-12-03 17:54:23 -0800

    create migration to add feedback column in triviasession

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202062720) do
+ActiveRecord::Schema.define(version: 20171204015147) do
 
   create_table "answers", force: :cascade do |t|
     t.text "description"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20171202062720) do
     t.integer "trivia_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "feedback"
   end
 
   create_table "users", force: :cascade do |t|
