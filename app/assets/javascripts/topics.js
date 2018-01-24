4a669977ed7e0cab8165ac6c2a4619e8b23ad4fd
Soumya Veer <veer.soumya@gmail.com>
2018-01-23 17:53:19 -0800

    render topics#index using javaScript

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  var topicsUrl = "/topics" + ".json";
+  $.get(topicsUrl, function(json){
+    json.data.forEach(function(topic){
+      var topicTriviasUrl = "/topics/" + topic.id + "/trivias";
+      var topicName = topic.attributes.name;
+      $(".js-topic").append("<a href=" + topicTriviasUrl + ">" +
+        topicName + "</a>" + "<br/>");
+    });
+  });
 });
+
+
