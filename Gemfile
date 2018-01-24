ba7324fec735a22bfb9bee4b93ee86309d436b4c
Soumya Veer <veer.soumya@gmail.com>
2018-01-23 22:20:36 -0800

    fix topics.js

--- Gemfile
+++ Gemfile
@@ -24,7 +24,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
 gem 'devise'
 gem "omniauth"
 gem "omniauth-facebook"
--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
   var topicsUrl = "/topics" + ".json";
-  $.get(topicsUrl, function(json){
+  $.getJSON(topicsUrl, function(json){
     json.data.forEach(function(topic){
-      var topicTriviasUrl = "/topics/" + topic.id + "/trivias";
+      var topicsTriviasUrl = "/topics/" + topic.id + "/trivias";
       var topicName = topic.attributes.name;
-      $(".js-topic").append("<a href=" + topicTriviasUrl + ">" +
-        topicName + "</a>" + "<br/>");
+      $(".js-topics").append("<li class='topic-display'><a href=" + topicsTriviasUrl + ">" + topicName + "</a></li>");
     });
   });
 });
