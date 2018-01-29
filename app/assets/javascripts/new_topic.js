248a10ec47e1a1f1babb8d1d2f15257bd60647b9
Soumya Veer <veer.soumya@gmail.com>
2018-01-29 12:44:54 -0800

    update new-topic.js to add Model objects & prototype function to it

--- app/assets/javascripts/new_topic.js
+++ app/assets/javascripts/new_topic.js
@@ -1,3 +1,15 @@
+function Topic(attributes) {
+  console.log(attributes);
+  this.id = attributes.id;
+  this.name = attributes.name;
+}
+
+Topic.prototype.renderName = function(){
+  var newTopicsTriviaUrl = "/topics/" + this.id + "/trivias/new";
+  $("#topicName").append("<a href=" + newTopicsTriviaUrl + ">" + this.name +"</a>" );
+};
+
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
@@ -5,10 +17,8 @@ $(document).ready(function(){
     var values = $(this).serialize();
     var posting = $.post('/topics', values);
     posting.done(function (data){
-      var topicId = data.topic.id;
-      var topicName = data.topic.name;
-      var newTopicsTriviaUrl = "/topics/" + topicId + "/trivias/new";
-      $("#topicName").append("<a href=" + newTopicsTriviaUrl + ">" + topicName +"</a>" );
+      var topic = new Topic(data.topic);
+      topic.renderName();
     });
 
   });
