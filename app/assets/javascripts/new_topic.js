f06d183daed7820cce7d10dffbcdd332f17702eb
Soumya Veer <veer.soumya@gmail.com>
2018-01-29 14:51:57 -0800

    fix new-topic.js

--- app/assets/javascripts/new_topic.js
+++ app/assets/javascripts/new_topic.js
@@ -1,25 +1,23 @@
-function Topic(attributes) {
-  console.log(attributes);
+function Topic(attributes){
   this.id = attributes.id;
   this.name = attributes.name;
 }
 
 Topic.prototype.renderName = function(){
-  var newTopicsTriviaUrl = "/topics/" + this.id + "/trivias/new";
-  $("#topicName").append("<a href=" + newTopicsTriviaUrl + ">" + this.name +"</a>" );
+  // http://localhost:3000/topics/3/trivias/new
+  var newTriviaUrl = "/topics/" + this.id + "/trivias/new";
+  $("#topicName").append("<a href=" + newTriviaUrl + ">" + this.name + "</a>");
 };
 
-
 $(document).ready(function(){
-  $("form #js-form").submit(function(event){
-    event.preventDefault();
+  $("form").submit(function(event){
+   event.preventDefault();
+   var values = $(this).serialize();
+   var posting = $.post("/topics", values);
 
-    var values = $(this).serialize();
-    var posting = $.post('/topics', values);
-    posting.done(function (data){
-      var topic = new Topic(data.topic);
-      topic.renderName();
-    });
-
+   posting.done(function(data){
+     var topic = new Topic(data.topic);
+     topic.renderName();
+   });
   });
 });
