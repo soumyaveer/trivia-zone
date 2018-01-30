18f0b7f75feb23a49a30dafbab969812708106bc
Soumya Veer <veer.soumya@gmail.com>
2018-01-29 17:05:07 -0800

    add test for TopicsController#show for json format

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -50,8 +50,8 @@ describe TopicsController do
 
           json_response = JSON.parse(response.body).deep_symbolize_keys
           expect(json_response[:topics]).to eql([{id: topic_1.id , name: topic_1.name, trivias: []},
-                                                          {id: topic_2.id , name: topic_2.name, trivias: []},
-                                                          {id: topic_3.id , name: topic_3.name, trivias: []}]);
+                                                 {id: topic_2.id , name: topic_2.name, trivias: []},
+                                                 {id: topic_3.id , name: topic_3.name, trivias: []}]);
         end
       end
     end
@@ -87,38 +87,68 @@ describe TopicsController do
         sign_in(@current_user, scope: :user)
       end
 
-      it 'should render template index' do
-        topic = FactoryBot.create(:topic)
+      context 'when format is HTML' do
+        it 'should render template index' do
+          topic = FactoryBot.create(:topic)
 
-        get :show, params: { id: topic }
+          get :show, params: { id: topic }
 
-        expect(response).to render_template('show')
+          expect(response).to render_template('show')
+        end
+
+        it 'returns all the played trivias for the topic' do
+          topic_1 = FactoryBot.create(:topic)
+          topic_2 = FactoryBot.create(:topic)
+          trivia_1 = create_trivia(6, topic_1)
+          trivia_2 = create_trivia(10, topic_1)
+          create_trivia(10, topic_2)
+
+          get :show, params: { id: topic_1 }
+
+          expect(assigns(:trivias)).to match_array([trivia_1, trivia_2])
+        end
+
+        it 'should not return the trivias of another topic' do
+          topic_1 = FactoryBot.create(:topic)
+          topic_2 = FactoryBot.create(:topic)
+          trivia_1 = create_trivia(6, topic_1)
+          trivia_2 = create_trivia(10, topic_1)
+          trivia_3 = create_trivia(10, topic_2)
+
+          get :show, params: { id: topic_1 }
+
+          expect(assigns(:trivias)).not_to match_array([trivia_1, trivia_2, trivia_3])
+        end
       end
 
-      it 'returns all the played trivias for the topic' do
-        topic_1 = FactoryBot.create(:topic)
-        topic_2 = FactoryBot.create(:topic)
-        trivia_1 = create_trivia(6, topic_1)
-        trivia_2 = create_trivia(10, topic_1)
-        create_trivia(10, topic_2)
+      context 'when format is JSON' do
+        it 'response should be 200' do
+          topic = FactoryBot.create(:topic)
 
-        get :show, params: { id: topic_1 }
+          get :show, params: { id: topic }, format: :json
 
-        expect(assigns(:trivias)).to match_array([trivia_1, trivia_2])
-      end
+          expect(response.code).to eql("200")
+        end
 
-      it 'should not return the trivias of another topic' do
-        topic_1 = FactoryBot.create(:topic)
-        topic_2 = FactoryBot.create(:topic)
-        trivia_1 = create_trivia(6, topic_1)
-        trivia_2 = create_trivia(10, topic_1)
-        trivia_3 = create_trivia(10, topic_2)
+        it 'returns all the played trivias for the topic in JSON format' do
+          topic_1 = FactoryBot.create(:topic)
+          topic_2 = FactoryBot.create(:topic)
+          trivia_1 = FactoryBot.create(:trivia, topic: topic_1)
+          trivia_2 = FactoryBot.create(:trivia, topic: topic_1)
+          FactoryBot.create(:trivia, topic: topic_2)
 
-        get :show, params: { id: topic_1 }
+          get :show, params: { id: topic_1 }, format: :json
 
-        expect(assigns(:trivias)).not_to match_array([trivia_1, trivia_2, trivia_3])
+          json_response = JSON.parse(response.body).deep_symbolize_keys
+
+          expect(json_response[:topic][:id]).to eql(topic_1.id)
+          expect(json_response[:topic][:name]).to eql(topic_1.name)
+          expect(json_response[:topic][:trivias][0][:id]).to eql(trivia_1.id)
+          expect(json_response[:topic][:trivias][1][:id]).to eql(trivia_2.id)
+          expect(json_response[:topic][:trivias][0][:title]).to eql(trivia_1.title)
+          expect(json_response[:topic][:trivias][1][:title]).to eql(trivia_2.title)
+        end
       end
     end
   end
-
 end
